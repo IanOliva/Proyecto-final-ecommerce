8f19fb8348e1e4e0154077f83e36954a78e67368
IanOliva <ianoliva2000@gmail.com>
2023-10-29 17:21:25 -0300

    paginacion y busqueda

--- index.php
+++ index.php
@@ -56,10 +56,11 @@
                                 <i class="fas fa-search"></i>
                             </a>
                             <div class="navbar-search-block">
-                                <form class="form-inline">
+                                <form class="form-inline" action="index.php">
                                     <div class="input-group input-group-sm">
                                         <input class="form-control form-control-navbar" type="search"
-                                            placeholder="Search" aria-label="Search">
+                                            placeholder="Search" aria-label="Search" name="nombre" value="<?php echo $_REQUEST['nombre']??'';?>">
+                                            <input type="hidden" name="modulo" value="productos">
                                         <div class="input-group-append">
                                             <button class="btn btn-navbar" type="submit">
                                                 <i class="fas fa-search"></i>
@@ -177,8 +178,33 @@
                     <?php 
                         include_once "admin/DBecommerce.php";
                         $conexion=mysqli_connect($host,$user,$password,$db);
-                        $query="SELECT id,nombre,precio,stock,imagenes FROM productos";
-                        $res=mysqli_query($conexion,$query);
+                        $where=" where 1=1 ";
+                        $nombre= mysqli_real_escape_string($conexion,$_REQUEST['nombre']??'');
+                        if( empty($nombre)==false ){
+                            $where="where nombre like '%".$nombre."%'";
+                        }
+                        $queryCuenta="SELECT COUNT(*) as cuenta FROM productos  $where ;";
+                        $resCuenta=mysqli_query($conexion,$queryCuenta);
+                        $rowCuenta=mysqli_fetch_assoc($resCuenta);
+                        $totalRegistros=$rowCuenta['cuenta'];
+    
+                        $elementosPorPagina=6;
+    
+                        $totalPaginas=ceil($totalRegistros/$elementosPorPagina);
+    
+                        $paginaSel=$_REQUEST['pagina']??false;
+    
+                        if($paginaSel==false){
+                            $inicioLimite=0;
+                            $paginaSel=1;
+                        }else{
+                            $inicioLimite=($paginaSel-1)* $elementosPorPagina;
+                        }
+                        $limite=" limit $inicioLimite,$elementosPorPagina ";
+                       
+
+                        $query="SELECT id,nombre,precio,stock,imagenes FROM productos $where $limite";
+                        $res=mysqli_query($conexion,$query);                            
                         while ($row=mysqli_fetch_assoc($res)) {
 
                             $path= "admin/".$row['imagenes']; //ubicacion de las imagenes
@@ -190,7 +216,7 @@
                                 <h4 class="card-title "><?php echo $row ['nombre'] ?> </h4>
                                 <p class="card-text"><strong>Precio: </strong><?php echo $row ['precio'] ?></p>
                                 <p class="card-text"><strong>Stock: </strong><?php echo $row ['stock'] ?> </p>
-                                <a class="btn btn-primary" href="#" role="button"></a>
+                                <a class="btn btn-primary" href="#" role="button" >Ver</a>
                               </div>
                             </div>
                         </div>
@@ -200,6 +226,49 @@
                      ?>
                     
                 </div>
+                <?php
+                if($totalPaginas>0){
+                ?>
+                    <nav aria-label="Page navigation">
+                      <ul class="pagination">
+                        <?php
+                            if( $paginaSel!=1 ){
+                        ?>
+                        <li class="page-item">
+                          <a class="page-link" href="index.php?modulo=productos&pagina=<?php echo ($paginaSel-1); ?>" aria-label="Previous">
+                            <span aria-hidden="true">&laquo;</span>
+                            <span class="sr-only">Previous</span>
+                          </a>
+                        </li>
+                        <?php
+                        }
+                        ?>
+
+                        <?php
+                        for($i=1;$i<=$totalPaginas;$i++){
+                        ?>
+                        <li class="page-item <?php echo ($paginaSel==$i)?" active ":" "; ?>">
+                            <a class="page-link" href="index.php?modulo=productos&pagina=<?php echo $i; ?>"><?php echo $i; ?></a>
+                        </li>
+                        <?php
+                        }
+                        ?>
+                        <?php
+                            if( $paginaSel!=$totalPaginas ){
+                        ?>
+                        <li class="page-item">
+                          <a class="page-link" href="index.php?modulo=productos&pagina=<?php echo ($paginaSel+1); ?>" aria-label="Next">
+                            <span aria-hidden="true">&raquo;</span>
+                            <span class="sr-only">Next</span>
+                          </a>
+                        </li>
+                        <?php
+                            }
+                        ?>
+               
+                <?php
+                }
+                 ?>
             </div>
         </div>
 
